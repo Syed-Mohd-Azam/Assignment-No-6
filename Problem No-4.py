8e3c7e8326d2bcdb1e2c2ea64f796a3bf5d4ac88
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2022-10-10 01:55:29 +0530

    Update Problem No-4.py

--- Problem No-4.py
+++ Problem No-4.py
@@ -1,4 +1,4 @@
-# Write a python script to print greater between two numbers
+# Write a python script to print greater between two numbers.Print number only once even if the numbers are the same.
 print("Enter two numbers: ")
 a,b= int(input()) , int(input())
 print("Greater number is : ", a) if a>=b else print("Greater number is :", b)
