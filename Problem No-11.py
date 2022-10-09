70c6d9cf5ba2518217f59bebaf8e1b8aa915ab7b
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2022-10-10 01:53:34 +0530

    Update Problem No-11.py

--- Problem No-11.py
+++ Problem No-11.py
@@ -1,3 +1,4 @@
+# Write a python script to take the month value in numeric format and display the number of days in it.
 print(" Enter month number : ")
 month=int(input())
 if month in (1,3,5,7,8,10,12):
